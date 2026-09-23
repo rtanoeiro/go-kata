49c4cb32e05b9cc7ee335e83e16a6292edff768a
rtanoeiro <ramontanoeiro@gmail.com>
2026-09-23 17:38:16 +1000

    solution for first kata

--- 01-context-cancellation-concurrency/01-concurrent-aggregator/solution/aggregator.go
+++ 01-context-cancellation-concurrency/01-concurrent-aggregator/solution/aggregator.go
@@ -2,45 +2,104 @@ package aggregator
 
 import (
 	"context"
-	"log"
+	"log/slog"
 	"time"
 
 	"github.com/medunes/go-kata/01-context-cancellation-concurrency/01-concurrent-aggregator/order"
 	"github.com/medunes/go-kata/01-context-cancellation-concurrency/01-concurrent-aggregator/profile"
+	"golang.org/x/sync/errgroup"
 )
 
 type UserAggregator struct {
 	ProfileService  profile.Service
 	OrdersService   order.Service
-	Context         context.Context
-	TimeoutDuration int
-	Logger          log.Logger
+	TimeoutDuration time.Duration
+	Logger          *slog.Logger
 }
 
 type Option func(userAgg *UserAggregator)
 
-func WithLogger(logger log.Logger) {
-
+func WithLogger(logger *slog.Logger) Option {
+	return func(userAgg *UserAggregator) {
+		userAgg.Logger = logger
+	}
 }
 
-func WithTimeout(int) {
+func WithTimeout(timeoutDuration time.Duration) Option {
+	return func(userAgg *UserAggregator) {
+		userAgg.TimeoutDuration = timeoutDuration
+	}
 
 }
 
 func NewUserAggregator(
-	profileService profile.Service,
 	ordersService order.Service,
+	profileService profile.Service,
 	opts ...Option,
 ) *UserAggregator {
-	return &UserAggregator{
-		ProfileService:  profileService,
-		OrdersService:   ordersService,
-		Context:         context,
-		TimeoutDuration: timeoutDuration * int(time.Second),
-		Logger:          logger,
+	userAgg := &UserAggregator{}
+	userAgg.OrdersService = ordersService
+	userAgg.ProfileService = profileService
+
+	for _, opt := range opts {
+		opt(userAgg)
 	}
+	return userAgg
 }
 
-func (agg UserAggregator) Aggregate(id int) {
-	userId, _ := agg.ProfileService.Get(agg.Context, id)
+type AggregatedProfile struct {
+	Name string
+	Cost float64
+}
+
+func (agg UserAggregator) Aggregate(ctx context.Context, id int) ([]*AggregatedProfile, error) {
+	localCtx, cancelCtx := context.WithCancel(ctx)
+	// If the Timeout is equal to 0, then context is cancelled instantly, not giving time
+	// for further operations to complete
+	if agg.TimeoutDuration > 0 {
+		localCtx, cancelCtx = context.WithTimeout(ctx, agg.TimeoutDuration)
+	}
+	defer cancelCtx()
+
+	errGroup := errgroup.Group{}
+
+	var aggProfile []*AggregatedProfile
+	var userProfile *profile.Profile
+	var userOrders []*order.Order
+	var errProfile, errOrders error
+
+	errGroup.Go(
+		func() error {
+			userProfile, errProfile = agg.ProfileService.Get(localCtx, id)
+			if errProfile != nil {
+				agg.Logger.Info("Failed to get user profile", "error", errProfile)
+				return errProfile
+			}
+			return nil
+		})
+	errGroup.Go(
+		func() error {
+			userOrders, errOrders = agg.OrdersService.GetAll(localCtx, id)
+			if errOrders != nil {
+				agg.Logger.Info("Failed to get user orders", "error", errOrders)
+				return errOrders
+			}
+			return nil
+		})
+
+	if err := errGroup.Wait(); err != nil {
+		agg.Logger.Info("Failed to get data, returning empty profile", "error", err)
+		return nil, err
+	}
+
+	for _, order := range userOrders {
+		agg.Logger.Info("Adding order to Aggregated Profile", "order", order)
+		aggProfile = append(aggProfile, &AggregatedProfile{
+			Name: userProfile.Name,
+			Cost: order.Cost,
+		})
+	}
+	agg.Logger.Info("aggregation complete successfully", "user_id", userProfile.Id)
+
+	return aggProfile, nil
 }
